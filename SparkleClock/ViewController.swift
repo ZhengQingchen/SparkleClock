9f93fae3258f59e4f64396dc078fc5315afc23e6
ZhengQingchen <mac@macdeMacBook-Pro.local>
2015-03-16 22:32:20 +0800

    修改了下格式

--- SparkleClock/ViewController.swift
+++ SparkleClock/ViewController.swift
@@ -15,8 +15,8 @@ class ViewController: UIViewController {
     var dateFormatter2: NSDateFormatter!
     
     @IBOutlet var backView: UIView!
-  @IBOutlet var clockLabel: UILabel!
-  @IBOutlet var tapGestureRecognizer: UITapGestureRecognizer!
+    @IBOutlet var clockLabel: UILabel!
+    @IBOutlet var tapGestureRecognizer: UITapGestureRecognizer!
     @IBOutlet var shimmeringView: FBShimmeringView!
   
     @IBOutlet var doubleTapGestureRecognizer: UITapGestureRecognizer!
@@ -40,70 +40,7 @@ class ViewController: UIViewController {
             self.backView.backgroundColor = UIColor.blackColor()
         })
     }
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    
-    timer = NSTimer.scheduledTimerWithTimeInterval(0.5, target: self, selector: Selector("updateClock"), userInfo: nil, repeats: true)
-    dateFormatter = NSDateFormatter()
-    dateFormatter.dateStyle = NSDateFormatterStyle.NoStyle
-    dateFormatter.timeStyle = NSDateFormatterStyle.MediumStyle
-    
-    shimmeringView.contentView = clockLabel
-    shimmeringView.shimmering = true
 
-    
-    shimmeringViewDate.contentView = dateLable
-    shimmeringViewDate.shimmering = true
-    
-    
-    shimmeringViewDate.shimmeringSpeed = 230.0 / (shimmeringView.frame.width / shimmeringViewDate.frame.width)
-    println("\(shimmeringViewDate.shimmeringSpeed)")
-    dateFormatter2 = NSDateFormatter()
-    dateFormatter2.dateStyle = NSDateFormatterStyle.FullStyle
-    
-    
-    tapGestureRecognizer.requireGestureRecognizerToFail(doubleTapGestureRecognizer)
-  }
-  
-    
-    func randomColor() -> UIColor {
-        
-        var hue = (CGFloat)(arc4random() % 256) / 256.0
-        var saturation = (CGFloat)(arc4random() % 128) / 256.0 + 0.5
-        var brightness = (CGFloat)(arc4random() % 128) / 256.0 + 0.5
-        
-        
-        return UIColor(hue: hue, saturation: saturation, brightness: brightness, alpha: 1)
-    }
-    
-  override func viewWillAppear(animated: Bool) {
-    updateClock()
-  }
-  
-  override func viewDidAppear(animated: Bool) {
-    super.viewDidAppear(animated)
-    
-    UIApplication.sharedApplication().idleTimerDisabled = true
-  }
-  
-  override func viewDidDisappear(animated: Bool) {
-    super.viewDidDisappear(animated)
-    
-    UIApplication.sharedApplication().idleTimerDisabled = false
-  }
-  
-  override func prefersStatusBarHidden() -> Bool {
-    return true
-  }
-  
-  func updateClock() {
-    var timeToDisplay = dateFormatter.stringFromDate(NSDate())
-    var dateToDisplay = dateFormatter2.stringFromDate(NSDate())
-    clockLabel.text = timeToDisplay
-    dateLable.text = dateToDisplay
-    dateLable.sizeToFit()
-  }
-  
   @IBAction func didTapView() {
     shimmeringView.shimmering = !shimmeringView.shimmering
     shimmeringViewDate.shimmering = !shimmeringViewDate.shimmering
@@ -122,5 +59,71 @@ class ViewController: UIViewController {
         })
     })
   }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        timer = NSTimer.scheduledTimerWithTimeInterval(0.5, target: self, selector: Selector("updateClock"), userInfo: nil, repeats: true)
+        dateFormatter = NSDateFormatter()
+        dateFormatter.dateStyle = NSDateFormatterStyle.NoStyle
+        dateFormatter.timeStyle = NSDateFormatterStyle.MediumStyle
+        
+        shimmeringView.contentView = clockLabel
+        shimmeringView.shimmering = true
+        
+        
+        shimmeringViewDate.contentView = dateLable
+        shimmeringViewDate.shimmering = true
+        
+        
+        shimmeringViewDate.shimmeringSpeed = 230.0 / (shimmeringView.frame.width / shimmeringViewDate.frame.width)
+        println("\(shimmeringViewDate.shimmeringSpeed)")
+        dateFormatter2 = NSDateFormatter()
+        dateFormatter2.dateStyle = NSDateFormatterStyle.FullStyle
+        
+        
+        tapGestureRecognizer.requireGestureRecognizerToFail(doubleTapGestureRecognizer)
+    }
+    
+    
+    func randomColor() -> UIColor {
+        
+        var hue = (CGFloat)(arc4random() % 256) / 256.0
+        var saturation = (CGFloat)(arc4random() % 128) / 256.0 + 0.5
+        var brightness = (CGFloat)(arc4random() % 128) / 256.0 + 0.5
+        
+        
+        return UIColor(hue: hue, saturation: saturation, brightness: brightness, alpha: 1)
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        updateClock()
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        UIApplication.sharedApplication().idleTimerDisabled = true
+    }
+    
+    override func viewDidDisappear(animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+        UIApplication.sharedApplication().idleTimerDisabled = false
+    }
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+    
+    func updateClock() {
+        var timeToDisplay = dateFormatter.stringFromDate(NSDate())
+        var dateToDisplay = dateFormatter2.stringFromDate(NSDate())
+        clockLabel.text = timeToDisplay
+        dateLable.text = dateToDisplay
+        dateLable.sizeToFit()
+    }
+    
 }
 
